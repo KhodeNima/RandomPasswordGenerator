f5b03f036b4485650b46e167be77bd007282a725
KhodeNima <khodenimacollab@gmail.com>
2023-08-21 11:30:21 +0330

    RandomPasswordGenerator Project | V : 2.10.0
    Filing :
    
        1. Added the New simulations file.
    
    Features:
    
        1. Added the Quit app option to the avaliable operation's.
        2. Added The loading_screen function to the simulations file.
        3. Added The welcome_screen function to the simulations file.
        4. Added The goodbye_screen function to the simulations file.
        5. Added the show_operations_list function to the Menu class.
        6. Added show_current_password function to the Menu class

--- Random_Password_Generator_Project/coding/database/menu_control.py
+++ Random_Password_Generator_Project/coding/database/menu_control.py
@@ -1,1 +1,39 @@
-from database.data import *
+from data import *
+
+
+class Menu:
+
+    def __init__(self, operations_numbers : list, operations_names : list , current_password : str = None):
+        self.operations_numbers = operations_numbers
+        self.operations_names = operations_names
+        self.current_password = current_password
+
+        if not operations_names:
+            raise ValueError("The argument for the parameter ( operations_names ) Is not provided.")
+        if not operations_numbers:
+            raise ValueError("The argument for the parameter ( operations_names ) Is not provided. ")          
+        if not type(operations_numbers) is list:
+            raise ValueError("The Parameter type for the argument ( argument_numbers ) Is not valid.")
+        if not type(operations_names) is list:
+            raise ValueError("The parameter type for the argument ( operations_names ) Is not valid.")         
+        
+    def show_operations_list(self) -> str:
+        """Show a list of a all avaliable operation's Refrenced by their number's
+        """
+
+        exponential_index = 0
+        operations_length = len(self.operations_names)
+
+        while exponential_index < operations_length:     
+            print(f'{color_red}{self.operations_numbers[exponential_index]}{color_blue} : {color_green}{self.operations_names[exponential_index]}{color_blue}')
+            exponential_index += 1
+
+    def show_current_password(self):
+        """Show the current generated password
+        """
+
+        if self.current_password == None:
+            print("No generated password.")
+            return None
+        
+        print(f"{color_blue} Your current Password Is : {color_green}{self.current_password} {color_blue}")

4bfbb65d67b36961076a8485911214071272b1f0
KhodeNima <khodenimacollab@gmail.com>
2023-08-31 08:52:19 +0330

    RandomPasswordGenerator Project | V : 3.5.2
    Patches:
    
        1. Patched the functionality of _generate Function ( Removed random and used secret module  Due to security issues ).
    
        2. Removed the reset_all_variables Function since it was causing functionality Problem's.
    
    
    Features:
    
        1. Implemented the lowercase RandomPassword generation feature.
    
        2. Implemented the uppercase RandomPassword generation feature.
    
        3. Implemented the numeric RandomPassword generation feature.
    
        4. Implemented the mixed RandomPassword generation feature.

--- Random_Password_Generator_Project/code_package/main.py
+++ Random_Password_Generator_Project/code_package/main.py
@@ -1,9 +1,13 @@
+"""RandomPasswordGenerator : By KhodeNima
+2023/8/31
+"""
+
+
 from database.data import *
 from database.input_control import *
 from database.logic import *
 from database.menu_control import *
 from database.simulations import *
-
 
 
 def main():
@@ -16,31 +20,58 @@ def main():
     current_input = InputControl()
 
     while True:
-
-        reset_all_variables()
         current_menu.show_operations_list()
-        current_menu.evaluate_new_password(OperationalVariables.generated_random_password_string)
+        current_menu.evaluate_new_password(generate_random_password())
         current_menu.show_current_password()
 
         current_input.input_desired_operation(
-            f'{color_blue} Please enter your desired operation{color_red} Number : {color_blue} '
-            )
+            f"{color_blue} Please enter your desired operation{color_red} Number : {color_blue} "
+        )
 
         if OperationalVariables.operation_is_invalid:
             clean()
-            print(f'{color_red} This operation number is invalid! Please try again! {color_blue}')
+            print(
+                f"{color_red} This operation number is invalid! Please try again! {color_blue}"
+            )
             sleep(3)
             continue
-        
-        clean()
-        current_input.input_desired_length(
-            input_text=f"{color_blue} Please enter the length for your password {color_red}( 1 TO 20 ){color_blue} : "
+
+        if OperationalVariables.decided_to_quit:
+            InputControl.confirm_quit_input(
+                f"{color_red} Are you sure you would like to quit ? ( Yes or no only ) : "
             )
-            
-        
-        
+
+            if OperationalVariables.input_confirm_to_quit.lower() == "no":
+                clean()
+                print("Returning to the main menu then.")
+                sleep(4)
+                continue
+
+            if (
+                OperationalVariables.confirmation_is_invalid
+                and not OperationalVariables.confirmed_to_quit
+            ):
+                clean()
+                print(f"That answer Is invalid , Diverting to the menu...{color_blue}")
+                sleep(4)
+                continue
+
+            if (
+                OperationalVariables.confirmation_is_valid
+                and OperationalVariables.confirmed_to_quit
+            ):
+                goodbye_screen()
+                sys.exit()
+
+        current_input.input_desired_length(
+            "Please enter the lengh for your password : "
+        )
+
+        if OperationalVariables.length_is_invalid:
+            clean()
+            print(f"{color_red} The entered lengh is not valid , Please try again.")
+            sleep(5)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
